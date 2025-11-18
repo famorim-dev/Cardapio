164c492a65cbb27f90c67e2452205d1465a78fc0
Felipe Santos de Amorim <255582023@eniac.edu.br>
2025-11-18 14:53:00 -0300

    perfil completo

---
+++ perfil/hook/buscarUsuario.js
@@ -0,0 +1,20 @@
+import { API } from '../../utils/api.js';
+
+export async function getUserOrders(token) {
+    try {
+        const response = await fetch(`${API}/pedido/buscar`, {
+            headers: { "Authorization": `Bearer ${token}` }
+        });
+
+        if (!response.ok) throw new Error("Não foi possível carregar os pedidos.");
+
+        const orders = await response.json();
+
+        const profile = orders.length > 0 ? orders[0].usuario : null;
+
+        return { profile, orders };
+    } catch (err) {
+        console.error(err);
+        return { profile: null, orders: [] };
+    }
+}
